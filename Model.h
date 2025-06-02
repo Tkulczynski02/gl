f44ba5361f779ac49ab6c28d275ea9d43cb35b18
Tkulczynski02 <126588157+Tkulczynski02@users.noreply.github.com>
2025-06-02 19:54:51 +0200

    Update Model.h

--- Model.h
+++ Model.h
@@ -2,20 +2,19 @@
 
 #include <vector>
 #include <string>
-// Usun¹³em <map>, bo textures_loaded to vector. Jeœli u¿ywasz mapy gdzie indziej, przywróæ.
 #include <glm/glm.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-#include "Mesh.h"   // Zawiera teraz poprawn¹ definicjê Vertex i Texture
+#include "Mesh.h"   
 #include "Shader.h"
-#include "stb_image.h" // Potrzebne dla TextureFromFile
+#include "stb_image.h" 
 
 class Model
 {
 public:
-    std::vector<Texture> textures_loaded; // Przechowuje wszystkie tekstury za³adowane dla tego modelu
+    std::vector<Texture> textures_loaded; 
     std::vector<Mesh> meshes;
     std::string directory;
     bool gammaCorrection;
